8dcedf1e746f32197429b99adec6dd833ed5957b
bian <biancapicchio04@gmail.com>
2024-04-10 23:50:21 -0300

    multioperador

---
+++ src/ar/edu/unq/po2/tp3/Multioperador.java
@@ -0,0 +1,39 @@
+package ar.edu.unq.po2.tp3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Multioperador {
+	 
+	private List<Integer> numeros = new ArrayList<Integer>();
+	
+	public void addNumber(int n) {
+		numeros.add(n);
+	}
+	
+	public int suma () {
+		int suma = 0;
+		for (int numero : numeros) {
+			suma += numero;
+		}
+		return suma;
+	}
+	
+	/*public int resta () {
+		int resta = numeros.getFirst();
+		List<Integer> numerosSinPrimero = numeros.remove(0);
+		for (int numero : numerosSinPrimero) {
+			resta -= numero;
+		}
+		return resta;
+	}*/
+	
+	public int multiplicacion () {
+		int multiplicacion = 1;
+		for (int numero : numeros) {
+			multiplicacion = numero;
+		}
+		return multiplicacion;
+	}
+}
+ 
